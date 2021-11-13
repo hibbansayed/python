0b92559fd827bd432d6b163a7a0961aefcea1b9a
hibbansayed <94217226+hibbansayed@users.noreply.github.com>
2021-11-13 16:41:40 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-v1 = "hello"
-print((v1+"\n") * 5)   
+#customer bill calculator
+customer_name = input("Customer Name : ")
+customer_details = input("Table No : ")
+bill_total = int(input("Total : " ))
+before_tax_total = bill_total
+after_tax_total = (before_tax_total + 50)
+print("CUSTOMER DETAILS")
+print("Customer Name: "+customer_name,"\n","Table No: "+customer_details)
+print("Before Tax: " + str(before_tax_total) )
+print(f"After Tax:  {after_tax_total}")
+print("Grand Total: " + str(after_tax_total))
